80c187c9a8d18b158073cc10a885016c5cc63c25
megan471 <megan@bundlemedia.africa>
2026-06-08 10:37:46 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/fvh-shift-manager/", // must match your GitHub repo name exactly
-});
